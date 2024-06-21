3030d19b9cb1c48f34dace25689a99bbec334d55
kythong <kythong2003@gmail.com>
2024-06-21 13:13:08 +0700

    bbbbbbbbbbbbbbb

--- lib/config/api_service.dart
+++ lib/config/api_service.dart
@@ -9,6 +9,6 @@ Future<List<Movies>> fetchMovies() async {
     List<dynamic> data = json.decode(response.body);
     return data.map((json) => Movies.fromJson(json)).toList();
   } else {
-    throw Exception('Failed to load products');
+    throw Exception('Failed to load productbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbs');
   }
 }
